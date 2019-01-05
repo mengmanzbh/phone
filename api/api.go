a450fea7bd012cc908a4ccd50721340beb099d49
mengmanzbh <1361006944@qq.com>
2019-01-05 17:15:30 +0800

    KO

--- api/api.go
+++ api/api.go
@@ -3,8 +3,11 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	// "fmt"
-	// "io/ioutil"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "fmt"
+    "encoding/json"
 )
 const APPKEY = "f17e30d841c5b17dbc00605a556d549a" //您申请的APPKEY
 // 检测手机号码是否能充值
@@ -61,4 +64,32 @@ func Onlineorder(ctx *gin.Context) {
 // 订单状态查询
 func Ordersta(ctx *gin.Context) {
 	
+}
+// get 网络请求
+func Get(apiURL string,params url.Values)(rs[]byte ,err error){
+    var Url *url.URL
+    Url,err=url.Parse(apiURL)
+    if err!=nil{
+        fmt.Printf("解析url错误:\r\n%v",err)
+        return nil,err
+    }
+    //如果参数中有中文参数,这个方法会进行URLEncode
+    Url.RawQuery=params.Encode()
+    resp,err:=http.Get(Url.String())
+    if err!=nil{
+        fmt.Println("err:",err)
+        return nil,err
+    }
+    defer resp.Body.Close()
+    return ioutil.ReadAll(resp.Body)
+}
+ 
+// post 网络请求 ,params 是url.Values类型
+func Post(apiURL string, params url.Values)(rs[]byte,err error){
+    resp,err:=http.PostForm(apiURL, params)
+    if err!=nil{
+        return nil ,err
+    }
+    defer resp.Body.Close()
+    return ioutil.ReadAll(resp.Body)
 }
