0ea455159546eb436487a9bfcbbbf1ac80733a7f
mengmanzbh <1361006944@qq.com>
2019-01-05 17:36:30 +0800

    OK

--- api/api.go
+++ api/api.go
@@ -24,7 +24,7 @@ func Telcheck(ctx *gin.Context) {
  
  
     //发送请求
-    data,err:=Get(juheURL,param)
+    data,err:=POST(juheURL,param)
     if err!=nil{
         fmt.Errorf("请求失败,错误信息:\r\n%v",err)
         ctx.JSON(404, gin.H{
