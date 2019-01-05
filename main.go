40fcb08205ccf8bd8c6c8c768e9f0364cb76d59a
mengmanzbh <1361006944@qq.com>
2019-01-05 10:25:37 +0800

    OK

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 // @BasePath /v2
 func main() {
 	r := gin.New()
-	r.GET("/testapi/get-string-by-int/:some_id", api.GetStringByInt)
+	r.POST("/testapi/get-string-by-int/:some_id", api.GetStringByInt)
 	r.GET("/testapi/get-struct-array-by-string/:some_id", api.GetStructArrayByString)
 	r.POST("/testapi/upload", api.Upload)
 
