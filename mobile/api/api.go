2901bca81f8cdc4dcd2fff18987aa83c4f1be2b5
mengmanzbh <1361006944@qq.com>
2019-02-12 16:23:24 +0800

    OK

--- mobile/api/api.go
+++ mobile/api/api.go
@@ -121,5 +121,36 @@ func Onlineorder(ctx *gin.Context) {
 }
 // 订单状态查询
 func Ordersta(ctx *gin.Context) {
-	// orderid := ctx.PostForm("orderid")
+	orderid := ctx.PostForm("orderid")
+
+   //请求地址
+    juheURL :="http://op.juhe.cn/ofpay/mobile/ordersta"
+ 
+    //初始化参数
+    param:=url.Values{}
+ 
+    //配置请求参数,方法内部已处理urlencode问题,中文参数可以直接传参
+    param.Set("orderid",orderid) //商家订单号，8-32位字母数字组合
+    param.Set("key",APPKEY) //应用APPKEY(应用详细页查询)
+ 
+
+    //发送请求
+    data,err:=utils.Post(juheURL,param)
+    if err!=nil{
+        fmt.Errorf("请求失败,错误信息:\r\n%v",err)
+        ctx.JSON(404, gin.H{
+            "code": "404",
+            "message": err,
+            })
+    }else{
+        var netReturn map[string]interface{}
+        json.Unmarshal(data,&netReturn)
+
+        ctx.JSON(200, gin.H{
+            "error_code": netReturn["error_code"],
+            "message": netReturn["reason"],
+            "result":netReturn["result"],
+        })
+    }
+
 }
