e9b24c78c6bcf9f7bba788f5a94f10173692d82d
mengmanzbh <1361006944@qq.com>
2019-02-12 17:02:17 +0800

    OK

--- mobile/main.go
+++ mobile/main.go
@@ -13,7 +13,6 @@ func main() {
 	r.POST("/mobile/telquery", api.Telquery)
 	r.POST("/mobile/onlineorder", api.Onlineorder)
 	r.POST("/mobile/ordersta", api.Ordersta)
-	r.POST("/flow/list", api.Flowlist)
 
 	r.Run()
 }
